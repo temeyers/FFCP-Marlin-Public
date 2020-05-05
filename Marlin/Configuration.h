d5663676a1336a1a0554b565b038a9d9e95878c7
Terry Meyers <temeyers@gmail.com>
2020-05-05 11:12:19 -0500

    Increase Squish Factor by .5

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -949,7 +949,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 40, 0, -1.31 }
+#define NOZZLE_TO_PROBE_OFFSET { 40, 0, -1.81 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
